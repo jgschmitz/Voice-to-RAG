c3e467a1a72cf7bf2fa0d137df1440821362437c
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2025-04-07 14:00:25 -0500

    Create GradioFrontEnd.py

---
+++ GradioFrontEnd.py
@@ -0,0 +1,10 @@
+import gradio as gr
+from rag_core import generate_rag_response
+from voice_utils import transcribe_audio
+
+def handle_input(audio):
+    file_path = audio
+    query = transcribe_audio(file_path)
+    return generate_rag_response(query)
+
+gr.Interface(fn=handle_input, inputs="microphone", outputs="text").launch()
